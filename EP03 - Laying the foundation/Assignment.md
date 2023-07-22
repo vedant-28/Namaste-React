12bb4e39b01878dd031ff8904a367d40f6b2f67e
Vedant <vedant10vaidya@gmail.com>
2023-07-22 22:08:37 +0530

    Namaste React | EP03 - Laying the foundation | Coding updated + assignments completed

---
+++ EP03 - Laying the foundation/Assignment.md
@@ -0,0 +1,28 @@
+* Topics:
+JSX
+React.createElement vs JSX
+Benefits of JSX
+Behind the Scenes of JSX
+Babel & parcel role in JSX
+Components
+Functional Components
+Composing Components
+Theory Assignment:
+What is JSX?
+Superpowers of JSX.
+
+Role of type attribute in script tag? What options can I use there?
+{TitleComponent} vs {<TitleComponent/>} vs {<TitleComponent></TitleComponent>} in JSX.
+
+* Coding Assignment:
+1) Create a Nested header Element using React.createElement(h1,h2,h3 inside a div with class "title")
+2) Create the same element using JSX
+3) Create a functional component of the same with JSX
+4) Pass attribute into the tag in JSX
+5) Composition of Component (Add a component inside another)
+6) {TitleComponent} vs {<TitleComponent/>} vs {<TitleComponent></TitleComponent>} in JSX.
+7) Create a Header Component from scratch using Functional Component with JSX
+  - Add a Logo on Left
+  - Add a search bar in middle
+  - Add User icon on right
+  - Add CSS to make it look nice
--- EP03 - Laying the foundation/Code/App.js
+++ EP03 - Laying the foundation/Code/App.js
@@ -1,29 +1,61 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import searchIcon from "./assets/search-icon.svg";
+import userIcon from "./assets/user-profile-icon.png";
+import "./index.scss";
 
+// Coding assignment Q1: Nested elements using react.createElement:
 // React.createElement => gives React element, which is JS object => rendered as HTML element
-const heading = React.createElement("h1", { id: "heading" }, "Hello world!");
-console.log(heading);
+const heading = React.createElement("div", { className: "title" }, [
+  React.createElement("h1", {id: "h1", key:"h1"}, "h1 element from React.createElement()"),
+  React.createElement("h2", {id: "h2", key:"h2"}, "h2 element from React.createElement()"),
+  React.createElement("h3", {id: "h3", key:"h3"}, "h3 element from React.createElement()")
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 
+
+// Coding assignment Q2: Nested elements using JSX:
 // JSX => transpiled to React.createElement by Babel gives React element, which is JS object => rendered as HTML element
-const jsxHeading = <h1 id="jsx-heading">Hello world from JSX!</h1>;
-console.log(jsxHeading);
-
-root.render(jsxHeading);
-
-// React functinal component
-const Title = () => {
-  return <h1>Hello world from Title!</h1>;
-};
-
-const HeadingComp = () => (
-  <div>
-    <Title />
-    <h1>Hello world from Heading!</h1>
+const jsxHeading = (
+  <div className="title" key="titleJSX">
+    <h1 id="jsx-headingH1">h1 element from JSX</h1>
+    <h2 id="jsx-headingH2">h2 element from JSX</h2>
+    <h3 id="jsx-headingH3">h3 element from JSX</h3>
   </div>
 );
+//root.render(jsxHeading);
 
-root.render(<HeadingComp />);
+// Coding assignment Q3 & Q4 & Q5: Nested elements using JSX in functional component & passing attributes to jsx elements & component composition
+const CompComposition = () => (
+  <h1 style={{ textAlign: "center" }}>Component composition implementation</h1>
+);
+
+const JsxHeadingComp = () => (
+  <div className="title" key="titleJSX">
+    <CompComposition />
+    <h1 id="jsx-headingH1" style={{ color: "tomato" }}>h1 element from JSX</h1>
+    <h2 id="jsx-headingH2" style={{ color: "navy" }}>h2 element from JSX</h2>
+    <h3 id="jsx-headingH3" style={{ color: "green" }}>h3 element from JSX</h3>
+  </div>
+);
+//root.render(<JsxHeadingComp />);
+
+// Coding assignment Q6: Header component for search bar:
+const Header = () => (
+  <header className="search-bar-container" key="header-container">
+    <div className="left">
+      <img src={searchIcon} alt="search-icon" />
+    </div>
+    <div className="search-bar">
+      <input type="search" />
+      <button className="search-button">Search</button>
+    </div>
+    <div className="right">
+      <img src={userIcon} alt="user-icon" />
+    </div>
+  </header>
+);
+
+root.render(<Header />);
