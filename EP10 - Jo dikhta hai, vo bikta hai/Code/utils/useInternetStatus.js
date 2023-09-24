23e74c0370fac916fb45e1a8f9f506aac687ed63
Vedant <vedant10vaidya@gmail.com>
2023-09-24 18:34:09 +0530

    Namaste React | EP10 - Jo dikhta hai, vo bikta hai | Initial commit

---
+++ EP10 - Jo dikhta hai, vo bikta hai/Code/utils/useInternetStatus.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+
+const useInternetStatus = () => {
+	const [internetStatus, setInternetStatus] = useState(true);
+
+	useEffect(() => {
+		addEventListener("offline", () => {
+			setInternetStatus(false);
+		});
+	}, []);
+
+	return internetStatus;
+}
+
+export default useInternetStatus;
